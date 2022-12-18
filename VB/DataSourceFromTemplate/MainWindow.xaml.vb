4ff253723f44e4b68897ca9a96c658f455d77e86
DevExpressExampleBot <fakeEmail>
2022-12-18 15:01:05 +0400

    Source auto update [skip ci]

--- VB/DataSourceFromTemplate/MainWindow.xaml.vb
+++ VB/DataSourceFromTemplate/MainWindow.xaml.vb
@@ -1,73 +1,81 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Media
 
 Namespace DataSouceFromTemplate
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private gdps As New List(Of GDPInfo)()
+        Private gdps As List(Of GDPInfo) = New List(Of GDPInfo)()
 
-		Public Sub New()
-			InitializeComponent()
-			gdps.Add(New GDPInfo("Russia", 1221.99, 2010, Colors.White, Color.FromRgb(0, 57, 166), Color.FromRgb(213, 43, 30)))
-			gdps.Add(New GDPInfo("Germany", 3280.53, 2010, Colors.Black, Color.FromRgb(221, 0, 0), Color.FromRgb(255, 206, 0)))
-			gdps.Add(New GDPInfo("Netherlands", 836.4, 2010, Color.FromRgb(174, 28, 40), Colors.White, Color.FromRgb(33, 70, 139)))
-			gdps.Add(New GDPInfo("Austria", 389.7, 2010, Color.FromRgb(237, 41, 57), Colors.White, Color.FromRgb(237, 41, 57)))
-			Me.DataContext = gdps
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            gdps.Add(New GDPInfo("Russia", 1221.99, 2010, Colors.White, Color.FromRgb(0, 57, 166), Color.FromRgb(213, 43, 30)))
+            gdps.Add(New GDPInfo("Germany", 3280.53, 2010, Colors.Black, Color.FromRgb(221, 0, 0), Color.FromRgb(255, 206, 0)))
+            gdps.Add(New GDPInfo("Netherlands", 836.4, 2010, Color.FromRgb(174, 28, 40), Colors.White, Color.FromRgb(33, 70, 139)))
+            gdps.Add(New GDPInfo("Austria", 389.7, 2010, Color.FromRgb(237, 41, 57), Colors.White, Color.FromRgb(237, 41, 57)))
+            DataContext = gdps
+        End Sub
+    End Class
 
-	Public Class GDPInfo
-'INSTANT VB NOTE: The field country was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private ReadOnly country_Conflict As String
-'INSTANT VB NOTE: The field gdp was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private ReadOnly gdp_Conflict As Double
-		Private ReadOnly flagColor1 As Color
-		Private ReadOnly flagColor2 As Color
-		Private ReadOnly flagColor3 As Color
-'INSTANT VB NOTE: The field year was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private ReadOnly year_Conflict As Integer
+    Public Class GDPInfo
 
-		Public ReadOnly Property Country() As String
-			Get
-				Return country_Conflict
-			End Get
-		End Property
-		Public ReadOnly Property GDP() As Double
-			Get
-				Return gdp_Conflict
-			End Get
-		End Property
-		Public ReadOnly Property FlagBrush1() As Brush
-			Get
-				Return New SolidColorBrush(flagColor1)
-			End Get
-		End Property
-		Public ReadOnly Property FlagBrush2() As Brush
-			Get
-				Return New SolidColorBrush(flagColor2)
-			End Get
-		End Property
-		Public ReadOnly Property FlagBrush3() As Brush
-			Get
-				Return New SolidColorBrush(flagColor3)
-			End Get
-		End Property
-		Public ReadOnly Property Year() As Integer
-			Get
-				Return year_Conflict
-			End Get
-		End Property
+        Private ReadOnly countryField As String
 
-		Public Sub New(ByVal country As String, ByVal gdp As Double, ByVal year As Integer, ByVal flagColor1 As Color, ByVal flagColor2 As Color, ByVal flagColor3 As Color)
-			Me.country_Conflict = country
-			Me.gdp_Conflict = gdp
-			Me.year_Conflict = year
-			Me.flagColor1 = flagColor1
-			Me.flagColor2 = flagColor2
-			Me.flagColor3 = flagColor3
-		End Sub
-	End Class
+        Private ReadOnly gdpField As Double
+
+        Private ReadOnly flagColor1 As Color
+
+        Private ReadOnly flagColor2 As Color
+
+        Private ReadOnly flagColor3 As Color
+
+        Private ReadOnly yearField As Integer
+
+        Public ReadOnly Property Country As String
+            Get
+                Return countryField
+            End Get
+        End Property
+
+        Public ReadOnly Property GDP As Double
+            Get
+                Return gdpField
+            End Get
+        End Property
+
+        Public ReadOnly Property FlagBrush1 As Brush
+            Get
+                Return New SolidColorBrush(flagColor1)
+            End Get
+        End Property
+
+        Public ReadOnly Property FlagBrush2 As Brush
+            Get
+                Return New SolidColorBrush(flagColor2)
+            End Get
+        End Property
+
+        Public ReadOnly Property FlagBrush3 As Brush
+            Get
+                Return New SolidColorBrush(flagColor3)
+            End Get
+        End Property
+
+        Public ReadOnly Property Year As Integer
+            Get
+                Return yearField
+            End Get
+        End Property
+
+        Public Sub New(ByVal country As String, ByVal gdp As Double, ByVal year As Integer, ByVal flagColor1 As Color, ByVal flagColor2 As Color, ByVal flagColor3 As Color)
+            countryField = country
+            gdpField = gdp
+            yearField = year
+            Me.flagColor1 = flagColor1
+            Me.flagColor2 = flagColor2
+            Me.flagColor3 = flagColor3
+        End Sub
+    End Class
 End Namespace
